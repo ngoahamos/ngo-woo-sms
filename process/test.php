7e0910753f24b0411bf69c1c23f93c793cc22b36
Amos <lifesoftdev@gmail.com>
2024-01-25 06:04:44 +0800

    add more testing

--- process/test.php
+++ process/test.php
@@ -8,8 +8,12 @@ $response["message"] = "Invalid Data Provided";
 $response["status"] = false;
 
 if (isset($_POST['send']) && isset($_POST["order_id"]) ) {
+    $response["message"] = "Order";
+    $response["status"] = true;
     $order_id = $_POST["order_id"];
-    $response['data'] =  get_post_meta($order_id);
+    $order = wc_get_order($order_id);
+    $response['order_keys'] = wcsn_order_get_keys( $order_id );
+    $response['data']  = apply_filters( 'woocommerce_email_order_meta_fields', array(), $false, $order );
     
 }
 
